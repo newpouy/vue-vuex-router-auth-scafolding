3e5de6bda70f5ad0f2123a11dbe16205b93ff81a
rabelais88 <sungryeolp@gmail.com>
2018-07-10 11:43:22 +0900

    lintrc

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base'
+  ],
+ }
